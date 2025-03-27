2bc0c980684dcdbef3899caee46489710b5d43ae
Keeko <787018661@qq.com>
2025-03-27 10:19:40 +0800

    loadMicroApp不成功; 尝试使用全量注册方式加载子应用 failed 02

--- main_app/vue2/src/config/microAppConfig.js
+++ main_app/vue2/src/config/microAppConfig.js
@@ -1,6 +1,14 @@
 import { registerMicroApps, start } from "qiankun";
 
+console.log("qiankun start");
+
 registerMicroApps([
+  {
+    name: "microVue2-01",
+    entry: "http://localhost:7700/subapp/sub-vue2/about",
+    container: "#all-micro-container",
+    activeRule: "/main/micro-vue2",
+  },
   {
     name: "micro-react-app1",
     entry: "http://localhost:8001",
@@ -9,4 +17,6 @@ registerMicroApps([
   },
 ]);
 
-start();
+start({
+  prefetch: false, // 取消预加载
+});
