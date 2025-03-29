4353154b5cd0bf60b01b84205acda7c68045674c
Keeko <787018661@qq.com>
2025-03-29 10:15:23 +0800

    [feat] vue2子应用跳转 react子应用 (history.pushState)

--- main_app/vue2/src/App.vue
+++ main_app/vue2/src/App.vue
@@ -12,16 +12,48 @@
 import CusHeader from "@/components/CusHeader.vue";
 
 export default {
-  name: 'FoundationApp',
+  name: "FoundationApp",
   components: {
-    CusHeader
-  }
-}
+    CusHeader,
+  },
+  // methods: {
+  //   bindCurrent() {
+  //     const path = window.location.pathname;
+  //     if (this.microApps.findIndex((item) => item.activeRule === path) >= 0) {
+  //       this.current = path;
+  //     }
+  //   },
+  //   listenRouterChange() {
+  //     const _wr = function (type) {
+  //       const orig = history[type];
+  //       return function () {
+  //         const rv = orig.apply(this, arguments);
+  //         const e = new Event(type);
+  //         e.arguments = arguments;
+  //         window.dispatchEvent(e);
+  //         return rv;
+  //       };
+  //     };
+  //     history.pushState = _wr("pushState");
+
+  //     window.addEventListener("pushState", this.bindCurrent);
+  //     window.addEventListener("popstate", this.bindCurrent);
+
+  //     this.$once("hook:beforeDestroy", () => {
+  //       window.removeEventListener("pushState", this.bindCurrent);
+  //       window.removeEventListener("popstate", this.bindCurrent);
+  //     });
+  //   },
+  // },
+  // mounted() {
+  //   this.listenRouterChange();
+  // },
+};
 </script>
 
 <style lang="scss" scoped>
 #foundation-app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   // text-align: left;
--- micro_app/sub-vue2/src/views/About.vue
+++ micro_app/sub-vue2/src/views/About.vue
@@ -2,6 +2,10 @@
   <div class="about">
     <h1>In Vue2, This is an about page</h1>
     <div>fundId: {{ fundId }}</div>
+    <button @click="gotoSubReact()">从当前子应用内跳转到`react`子应用</button>
+    <div>
+      <button @click="gotoSubReact(123)">从当前子应用内带参跳转到`react`子应用</button>
+    </div>
   </div>
 </template>
 
@@ -12,6 +16,15 @@ export default {
     fundId() {
       return this.$route.query.fundId || "--";
     }
+  },
+  methods: {
+    gotoSubReact(fundId) {
+      let reactBaseUrl = '/main/micro-react/about';
+      if (fundId) {
+        reactBaseUrl = `${reactBaseUrl}?fundId=${fundId}`;
+      }
+      history.pushState(null, 'sub-react', reactBaseUrl)
+    },
   }
 }
 </script>
